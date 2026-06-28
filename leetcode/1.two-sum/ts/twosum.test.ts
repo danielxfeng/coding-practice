fd9dca38b689188f861782a0cdc59a4ca9ed75e0
Daniel Feng <daniel.feng@wolt.com>
2026-06-28 20:59:52 +0300

    twosum ts

--- leetcode/1.two-sum/ts/twosum.test.ts
+++ leetcode/1.two-sum/ts/twosum.test.ts
@@ -1,14 +1,21 @@
 import { expect, test } from "vitest";
 import twoSum from "./twosum.js";
 
+interface testCase {
+    nums: number[],
+    target: number,
+    want: number[],
+}
+
 test("twosum {2, 7, 11, 15}, 9 -> [0, 1]", () => {
-    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
-});
+    const tcs: testCase[] = [
+        { nums: [2, 7, 11, 15], target: 9, want: [0, 1] },
+        { nums: [3, 2, 4], target: 6, want: [1, 2] },
+        { nums: [3, 3], target: 6, want: [0, 1] },
+        { nums: [], target: 0, want: [] },
+        { nums: [0, 4, 3, 0], target: 0, want: [0, 3] },
+    ]
 
-test("twosum {3, 2, 4}, 6 -> [1, 2]", () => {
-    expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
-});
-
-test("twosum {3, 3}, 6 -> [0, 1]", () => {
-    expect(twoSum([3, 3], 6)).toEqual([0, 1]);
+    for (const tc of tcs)
+        expect(twoSum(tc.nums, tc.target)).toEqual(tc.want)
 });
--- leetcode/1.two-sum/ts/twosum.ts
+++ leetcode/1.two-sum/ts/twosum.ts
@@ -1,4 +1,22 @@
 const twoSum = (nums: number[], target: number): number[] => {
+    return twoSumOptimized(nums, target);
+};
+
+// N, N
+const twoSumOptimized = (nums: number[], target: number): number[] => {
+    const m = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        const idx = m.get(target - nums[i]);
+        if (idx !== undefined) return [idx, i];
+        m.set(nums[i], i);
+    }
+
+    return [];
+}
+
+// N, 1
+const twoSumBruteForce = (nums: number[], target: number): number[] => {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
