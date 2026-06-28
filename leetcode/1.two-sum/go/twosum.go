74ad5343e887c0bf5ae3c8d39ad449ac1c797e28
Daniel Feng <daniel.feng@wolt.com>
2026-06-28 21:23:43 +0300

    two sum optimize go

--- leetcode/1.two-sum/go/twosum.go
+++ leetcode/1.two-sum/go/twosum.go
@@ -6,7 +6,7 @@ func twoSum(nums []int, target int) []int {
 
 // N, N
 func twoSumOptimized(nums []int, target int) []int {
-	m := make(map[int]int)
+	m := make(map[int]int, len(nums))
 
 	for i, v := range nums {
 		expected := target - v
