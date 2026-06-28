23f2dfcb4bdbad976026b57c28c8763b6daae9f8
Daniel Feng <daniel.feng@wolt.com>
2026-06-28 20:28:59 +0300

    twosum go

--- leetcode/1.two-sum/go/twosum.go
+++ leetcode/1.two-sum/go/twosum.go
@@ -1,6 +1,26 @@
 package twosum
 
 func twoSum(nums []int, target int) []int {
+	return twoSumOptimized(nums, target)
+}
+
+// N, N
+func twoSumOptimized(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for i, v := range nums {
+		expected := target - v
+		if index, ok := m[expected]; ok {
+			return []int{index, i}
+		}
+		m[v] = i;
+	}
+
+	return []int{}
+}
+
+// N2, 1
+func twoSumBruteForce(nums []int, target int) []int {
 	for i := range nums {
 		for j := i + 1; j < len(nums); j++ {
 			if nums[i]+nums[j] == target {
