34240ef469d1411d4e34487e112ad671b34e3e18
Daniel Feng <daniel.feng@wolt.com>
2026-06-29 20:51:18 +0300

    binarysearch add

---
+++ leetcode/704.binary-search/go/binarysearch.go
@@ -0,0 +1,15 @@
+package binarysearch
+
+func search(nums []int, target int) int {
+	return searchBruteForce(nums, target)
+}
+
+func searchBruteForce(nums []int, target int) int {
+	for i, v := range nums {
+		if v == target {
+			return i
+		}
+	}
+
+	return -1
+}
