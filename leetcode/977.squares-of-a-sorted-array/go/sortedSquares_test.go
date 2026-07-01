a68e0aa326401c87d90b451581acb200a4b8237c
Daniel Feng <daniel.feng@wolt.com>
2026-07-01 21:12:30 +0300

    sortedsquare go

--- leetcode/977.squares-of-a-sorted-array/go/sortedSquares_test.go
+++ leetcode/977.squares-of-a-sorted-array/go/sortedSquares_test.go
@@ -15,6 +15,8 @@ func TestSortedSquares(t *testing.T) {
 	tcs := []testcase{
 		{nums: []int{-4, -1, 0, 3, 10}, want: []int{0, 1, 9, 16, 100}},
 		{nums: []int{-7, -3, 2, 3, 11}, want: []int{4, 9, 9, 49, 121}},
+		{nums: []int{-3}, want: []int{9}},
+		{nums: []int{}, want: []int{}},
 	}
 
 	for _, tc := range tcs {
--- leetcode/977.squares-of-a-sorted-array/go/sortedsquares.go
+++ leetcode/977.squares-of-a-sorted-array/go/sortedsquares.go
@@ -1,11 +1,40 @@
 package sortedsquares
 
-import "sort"
+import (
+	"sort"
+)
 
 func sortedSquares(nums []int) []int {
-	return sortedSquaresBruteForce(nums)
+	return sortedSquaresOptimized(nums)
 }
 
+// N, N
+func sortedSquaresOptimized(nums []int) []int {
+	l := 0
+	r := len(nums) - 1
+	n := len(nums) - 1
+
+	arr := make([]int, len(nums))
+
+	for n >= 0 {
+		sl := nums[l] * nums[l]
+		sr := nums[r] * nums[r]
+
+		if sl > sr {
+			arr[n] = sl
+			l++
+		} else {
+			arr[n] = sr
+			r--
+		}
+
+		n--
+	}
+
+	return arr
+}
+
+// NLogN, N
 func sortedSquaresBruteForce(nums []int) []int {
 	arr := make([]int, 0, len(nums))
 
