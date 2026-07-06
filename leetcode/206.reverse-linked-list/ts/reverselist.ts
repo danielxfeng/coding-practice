140a56679bc4b67af51e139b05a89acd37d11aba
Daniel Feng <daniel.feng@wolt.com>
2026-07-06 20:52:06 +0300

    reverselist ts

---
+++ leetcode/206.reverse-linked-list/ts/reverselist.ts
@@ -0,0 +1,24 @@
+export class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
+
+const reverseList = (head: ListNode | null): ListNode | null => {
+    let prev: ListNode | null = null
+    let curr: ListNode | null = head
+
+    while (curr !== null) {
+        const next: ListNode | null = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+
+    return prev
+};
+
+export default reverseList
