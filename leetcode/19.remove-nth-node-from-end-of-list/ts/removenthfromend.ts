36a6a479c6cebdb1d8c72c2d102264b5ebe96476
Daniel Feng <daniel.feng@wolt.com>
2026-07-08 22:20:50 +0300

    remove nth from end ts

---
+++ leetcode/19.remove-nth-node-from-end-of-list/ts/removenthfromend.ts
@@ -0,0 +1,27 @@
+export class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
+
+const removeNthFromEnd = (head: ListNode | null, n: number): ListNode | null => {
+    const dummy: ListNode | null = new ListNode(0, head);
+
+    let curr = head
+    let prevSlow: ListNode | null = dummy
+
+    let i = 0
+    while (curr != null) {
+        if (i++ >= n) prevSlow = prevSlow!.next
+        curr = curr.next
+    }
+
+    prevSlow!.next = prevSlow!.next!.next
+
+    return dummy.next
+};
+
+export default removeNthFromEnd
