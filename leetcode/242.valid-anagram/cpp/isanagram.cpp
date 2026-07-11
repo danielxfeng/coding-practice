21080aa1a77626143d3f4cb76c501625cd4ea623
Daniel Feng <daniel.feng@wolt.com>
2026-07-11 20:25:33 +0300

    is anagram optimized

--- leetcode/242.valid-anagram/cpp/isanagram.cpp
+++ leetcode/242.valid-anagram/cpp/isanagram.cpp
@@ -1,11 +1,44 @@
 #include <string>
 #include <unordered_map>
+#include <array>
 
 class Solution
 {
 public:
     // N, K
     bool isAnagram(std::string s, std::string t)
+    {
+        return isAnagramArray(s, t);
+    }
+
+private:
+    bool isAnagramArray(std::string s, std::string t)
+    {
+        if (s.length() != t.length())
+            return false;
+
+        std::array<int, 26> arr;
+
+        for (auto c : s)
+            arr[dist(c)]++;
+
+        for (auto c : t)
+        {
+            auto i = dist(c);
+
+            arr[i]--;
+            if (arr[i] < 0)
+                return false;
+        }
+        return true;
+    };
+
+    int dist(char ch)
+    {
+        return ch - 'a';
+    }
+
+    bool isAnagramBasic(std::string s, std::string t)
     {
         if (s.length() != t.length())
             return false;
--- leetcode/242.valid-anagram/go/isanagram.go
+++ leetcode/242.valid-anagram/go/isanagram.go
@@ -2,6 +2,37 @@ package validanagram
 
 // N, K
 func isAnagram(s string, t string) bool {
+	return isAnagramArray(s, t)
+}
+
+func isAnagramArray(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	arr := make([]int, 26)
+
+	for _, ch := range s {
+		arr[dist(ch)]++
+	}
+
+	for _, ch := range t {
+		i := dist(ch)
+
+		arr[i]--
+		if arr[i] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func dist(ch rune) int {
+	return (int)(ch - 'a')
+}
+
+func isAnagramBasic(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
