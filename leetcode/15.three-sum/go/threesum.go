98cbf16d4c967986a726230f158c59418ee158de
Daniel Feng <daniel.feng@wolt.com>
2026-07-17 22:57:47 +0300

    three sum go

---
+++ leetcode/15.three-sum/go/threesum.go
@@ -0,0 +1,45 @@
+package threesum
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := make([][]int, 0)
+
+	for i := 0; i < len(nums)-2; i++ {
+		if nums[i] > 0 {
+			break
+		}
+
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left := i + 1
+		right := len(nums) - 1
+
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+
+			if sum < 0 {
+				left++
+			} else if sum > 0 {
+				right--
+			} else {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			}
+		}
+	}
+
+	return res
+}
