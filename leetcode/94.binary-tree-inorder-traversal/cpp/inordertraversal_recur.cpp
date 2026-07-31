b27843cd120884f3964dbe03e5cf11e978b41c3f
Daniel Feng <daniel.feng@wolt.com>
2026-07-31 21:12:23 +0300

    inordertraversal recur cpp

---
+++ leetcode/94.binary-tree-inorder-traversal/cpp/inordertraversal_recur.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    std::vector<int> inorderTraversal(TreeNode *root)
+    {
+        std::vector<int> res;
+
+        traversal(root, res);
+        return res;
+    }
+
+private:
+    void traversal(TreeNode *root, std::vector<int> &res)
+    {
+        if (root == nullptr)
+            return;
+
+        traversal(root->left, res);
+        res.push_back(root->val);
+        traversal(root->right, res);
+    }
+};
