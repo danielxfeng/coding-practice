110db4ce40f1ea58eca2dcc509a1726921f04e61
Daniel Feng <daniel.feng@wolt.com>
2026-07-31 20:34:23 +0300

    postorder traversal recur cpp

---
+++ leetcode/145.binary-tree-postorder-traversal/cpp/postordertraversal_recur.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    std::vector<int> postorderTraversal(TreeNode *root)
+    {
+        std::vector<int> res;
+
+        traversal(root, res);
+
+        return res;
+    }
+
+private:
+    void traversal(TreeNode *root, std::vector<int> &res)
+    {
+        if (root == nullptr)
+            return;
+
+        traversal(root->left, res);
+        traversal(root->right, res);
+        res.push_back(root->val);
+    }
+};
