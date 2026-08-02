e617ffb4e03b6daa6db6199f504d12ecebdc96ef
Daniel Feng <daniel.feng@wolt.com>
2026-08-02 15:18:09 +0300

    level order cpp

---
+++ leetcode/102.binary-tree-level-order-traversal/cpp/levelorder.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <deque>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> levelOrder(TreeNode *root)
+    {
+        std::vector<std::vector<int>> res;
+        std::deque<TreeNode *> deque;
+
+        if (root != nullptr)
+            deque.push_back(root);
+
+        while (!deque.empty())
+        {
+            const int size = deque.size();
+            std::vector<int> layer(size);
+
+            for (int i = 0; i < size; ++i)
+            {
+                auto curr = deque.front();
+                deque.pop_front();
+
+                layer[i] = curr->val;
+
+                if (curr->left != nullptr)
+                    deque.push_back(curr->left);
+                if (curr->right != nullptr)
+                    deque.push_back(curr->right);
+            }
+
+            res.push_back(layer);
+        }
+
+        return res;
+    }
+};
