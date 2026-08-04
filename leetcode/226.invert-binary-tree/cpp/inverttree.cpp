37dc305b2f9b526f9df1b4fa760acf7fe3b69d0d
Daniel Feng <daniel.feng@wolt.com>
2026-08-04 21:44:11 +0300

    invert tree cpp

---
+++ leetcode/226.invert-binary-tree/cpp/inverttree.cpp
@@ -0,0 +1,33 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        swapNodes(root);
+        return root;
+    }
+
+private:
+    void swapNodes(TreeNode *root)
+    {
+        if (root == nullptr)
+            return;
+
+        auto temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        swapNodes(root->left);
+        swapNodes(root->right);
+    }
+};
