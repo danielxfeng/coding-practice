c9e0ccfcfe0f6df863f7fd1ce8e642417c3c2056
Daniel Feng <daniel.feng@wolt.com>
2026-08-07 20:15:34 +0300

    binary tree paths cpp

---
+++ leetcode/257.binary-tree-paths/cpp/binarytreepaths.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    std::vector<std::string> binaryTreePaths(TreeNode *root)
+    {
+        return travelsal(root, "");
+    }
+
+private:
+    std::vector<std::string> travelsal(TreeNode *root, std::string path)
+    {
+        if (root == nullptr)
+            return {};
+
+        auto val = std::to_string(root->val);
+        if (path == "")
+            path = val;
+        else
+            path += "->" + val;
+
+        if (root->left == nullptr && root->right == nullptr)
+            return {path};
+
+        std::vector<std::string> res;
+
+        auto left = travelsal(root->left, path);
+        auto right = travelsal(root->right, path);
+
+        res.insert(res.end(), left.begin(), left.end());
+        res.insert(res.end(), right.begin(), right.end());
+
+        return res;
+    }
+};
