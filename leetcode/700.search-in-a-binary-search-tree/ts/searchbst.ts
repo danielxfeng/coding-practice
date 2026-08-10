0526f6f3b0dbae180d040ddb7b7ff2a766c956d3
Daniel Feng <daniel.feng@wolt.com>
2026-08-10 17:39:35 +0300

    search bst ts

---
+++ leetcode/700.search-in-a-binary-search-tree/ts/searchbst.ts
@@ -0,0 +1,22 @@
+import search from "../../704.binary-search/ts/binarysearch.js"
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+const searchBST = (root: TreeNode | null, val: number): TreeNode | null => {
+    if (root === null) return null
+    if (root.val === val) return root
+
+    const left = searchBST(root.left, val)
+    if (left !== null) return left
+
+    return searchBST(root.right, val)
+}
