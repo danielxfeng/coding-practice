2e5b217562e16002905901d791bcb94675230c5f
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-08-19 17:34:49 +0300

    permute unique ts

---
+++ leetcode/47.permutations-ii/ts/permuteunique.ts
@@ -0,0 +1,32 @@
+const permuteUnique = (nums: number[]): number[][] => {
+    const res: number[][] = []
+
+    const backtrace = (start: number) => {
+        if (start === nums.length) {
+            res.push([...nums])
+        }
+
+        const used = new Set<number>()
+
+        for (let i = start; i < nums.length; i++) {
+            if (used.has(nums[i])) continue
+
+            used.add(nums[i])
+
+            swap(nums, start, i)
+            backtrace(start + 1)
+            swap(nums, start, i)
+        }
+    }
+
+    backtrace(0)
+    return res
+}
+
+const swap = (nums: number[], a: number, b: number) => {
+    const temp = nums[a]
+    nums[a] = nums[b]
+    nums[b] = temp
+}
+
+export default permuteUnique
