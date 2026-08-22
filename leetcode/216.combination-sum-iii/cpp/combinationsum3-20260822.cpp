11f6b72191eddf8d04f1c2f4465cc1f6c51056f1
Daniel Feng <daniel.feng@wolt.com>
2026-08-22 22:30:43 +0300

    retry combination sum3

---
+++ leetcode/216.combination-sum-iii/cpp/combinationsum3-20260822.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> combinationSum3(int k, int n)
+    {
+        backtrack(k, n, 1);
+        return res_;
+    }
+
+    void backtrack(int k, int n, int start)
+    {
+        if (path_.size() == k)
+        {
+            if (sum_ == n)
+                res_.push_back(path_);
+            return;
+        }
+
+        for (int i = start; i <= 9; i++)
+        {
+            if (sum_ + i > n)
+                break;
+
+            sum_ += i;
+            path_.push_back(i);
+            backtrack(k, n, i + 1);
+            sum_ -= i;
+            path_.pop_back();
+        }
+    }
+
+private:
+    std::vector<std::vector<int>> res_{};
+    std::vector<int> path_{};
+    int sum_{};
+};
