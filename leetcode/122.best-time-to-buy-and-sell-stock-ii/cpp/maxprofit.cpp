d2696fd2d4ea535c11d27aff960e4550873985ea
Daniel Feng <daniel.feng@wolt.com>
2026-08-26 19:08:18 +0300

    max profit cpp

--- leetcode/122.best-time-to-buy-and-sell-stock-ii/cpp/maxprofit.cpp
+++ leetcode/122.best-time-to-buy-and-sell-stock-ii/cpp/maxprofit.cpp
@@ -5,6 +5,19 @@ class Solution
 public:
     int maxProfit(std::vector<int> &prices)
     {
-        
+        if (prices.empty())
+            return 0;
+
+        int res = 0;
+
+        for (auto i = 1; i < prices.size(); i++)
+        {
+            int profit = prices[i] - prices[i - 1];
+
+            if (profit > 0)
+                res += profit;
+        }
+
+        return res;
     }
 };
