2a0ede3f4e2efa9b7bb0a95257447ea82a59236b
Daniel Feng <daniel.feng@wolt.com>
2026-08-26 19:06:05 +0300

    max profit ts

--- leetcode/122.best-time-to-buy-and-sell-stock-ii/ts/maxprofit.ts
+++ leetcode/122.best-time-to-buy-and-sell-stock-ii/ts/maxprofit.ts
@@ -1,3 +1,13 @@
 const maxProfit = (prices: number[]): number => {
+    if (prices.length === 0) return 0
+    
+    let res = 0
 
+    for (let i = 1; i < prices.length; i++) {
+        const profit = prices[i] - prices[i - 1]
+
+        if (profit > 0) res += profit
+    }
+
+    return res
 }
